dd59dcd1cf9ae459d30587d5d518351ac5428abd
mansiyadav8 <mansiyadav.yadav3@gmail.com>
2026-01-04 09:35:06 +0530

    my own create react app created

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 /**
  * <div id="parent">
  *      Hello World from React
@@ -15,13 +18,13 @@
 
 
 const reactElement = React.createElement("div",{id:"parent"},["Hello World from React",
-                        React.createElement("div",{id:"child"},["This is child",
-                            React.createElement("h1",{id:"tag1"},"This is sibiling1"),
-                            React.createElement("h1",{id:"tag2"},"This is sibiling2")])
+                        React.createElement("div",{key:"child"},["This is child",
+                            React.createElement("h1", { key: 1 }, "Sibling 1"),
+                            React.createElement("h1", { key: 2 }, "Sibling 2")])
                         ]
                     );
 
 console.log(reactElement); //Returns Object
 
-const root = ReactDOM.createRoot(document.getElementById("root")); //create a root and assign it in reactDOM
+const root = createRoot(document.getElementById("root")); //create a root and assign it in reactDOM
 root.render(reactElement); // put object on page, if anything is in root it will be replaced
